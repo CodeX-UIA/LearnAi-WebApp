77cfafb4ee7e92236e444b3f594a28a369bbcf4b
harsh-mr <harshme78@gmail.com>
2022-11-24 11:38:33 +0530

    school reg

--- pages/api/controller/registerschool.js
+++ pages/api/controller/registerschool.js
@@ -5,29 +5,56 @@ import users from "../model/userSchema";
 connectDB();
 export default async function handler(req, res) {
   try {
-    let check1 = await users.findOne({ email: req.body.email });
-    if (check1) {
-        return res.send({message:"A user with this email id already exists"});
-    }
-    console.log(check1);
-
+    // let check1 = await users.findOne({ email: req.body.email });
+    // if (check1) {
+    //     return res.send({message:"A user with this email id already exists"});
+    // }
+    // console.log(check1);
+    console.log(req.body);
     const details = new school({
-      email: req.body.email,
-      zone: req.body.zone,
-      averageMarks:[],
-      averageReview:0,
-      infra:0,
+      email:req.body.email,
+      schoolName:req.body.schoolName,
+      schoolID:req.body.schoolID,
+      type:req.body.type,
+      language:req.body.language,
+      phone:req.body.phone,
+      address:req.body.address,
+      assemblyArea:req.body.amenities.assemblyArea,
+      auditorium:req.body.amenities.auditorium,
+      cafeteria:req.body.amenities.cafeteria,
+      electricity:req.body.amenities.electricity,
+      furniture:req.body.amenities.furniture,
+      libraries:req.body.amenities.libraries,
+      sanitation:req.body.amenities.sanitation,
+      scienceLabs:req.body.amenities.scienceLabs,
+      sportsFacilities:req.body.amenities.sportsFacilities,
+      water:req.body.amenities.water,
+      english:req.body.courses.english,
+      literature:req.body.courses.literature,
+      maths:req.body.courses.maths,
+      science:req.body.courses.science,
+      socialScience:req.body.courses.socialScience,
+      enrolledStudents:req.body.enrolledStudents,
+      amenitiesRating:req.body.amenitiesRating,
+      teachers:0,
+      enrolledStudents:0,
+      averageMarks:0,
+      averagetraveltime:0,
+      ParentalEducation:0,
+      safety:0,
+      avgIncome:0
     });
     details.save();
+    // res.json(req.body.amenities.furniture);
 
-    const use=new users({
-      email:req.body.email,
-      role:req.body.role
-  })
-  use.save()
+  //   const use=new users({
+  //     email:req.body.email,
+  //     role:req.body.role
+  // })
+  // use.save()
 
     res.send({ message: "Successfully registered" });
   } catch (err) {
-    res.send({ message: err });
+    res.send({ message: err});
   }
 }
--- pages/api/model/schoolSchema.js
+++ pages/api/model/schoolSchema.js
@@ -6,19 +6,119 @@ const schoolSchema = new mongoose.Schema({
         type:String,
         required:true
     },
-    zone:{
+    schoolName:{
         type:String,
         required:true
     },
-    averageMarks:{
-        type:Array,
+    schoolID:{
+        type:String,
         required:true
     },
-    averageReview:{
+    type:{
+        type:String,
+        required:true
+    },
+    language:{
+        type:String,
+        required:true
+    },
+    phone:{
+        type:String,
+        required:true
+    },
+    address:{
+        type:String,
+        required:true
+    },
+    assemblyArea:{
+        type:String,
+        required:true
+    },
+    auditorium:{
+        type:String,
+        required:true
+    },
+    cafeteria:{
+        type:String,
+        required:true
+    },
+    electricity:{
+        type:String,
+        required:true
+    },
+    furniture:{
+        type:String,
+        required:true
+    },
+    libraries:{
+        type:String,
+        required:true
+    },
+    sanitation:{
+        type:String,
+        required:true
+    },
+    scienceLabs:{
+        type:String,
+        required:true
+    },
+    sportsFacilities:{
+        type:String,
+        required:true
+    },
+    water:{
+        type:String,
+        required:true
+    },
+    english:{
+        type:String,
+        required:true
+    },
+    literature:{
+        type:String,
+        required:true
+    },
+    maths:{
+        type:String,
+        required:true
+    },
+    science:{
+        type:String,
+        required:true
+    },
+    socialScience:{
+        type:String,
+        required:true
+    },
+    amenitiesRating:{
         type:Number,
         required:true
-    },  
-    infra:{
+    },
+    teachers:{
+        type:Number,
+        required:true
+    },
+    enrolledStudents:{
+        type:Number,
+        required:true
+    },
+    averageMarks:{
+        type:Number,
+        required:true
+    },
+    averagetraveltime:{
+        type:Number,
+        required:true
+    },
+    ParentalEducation:{
+        type:Number,
+        required:true
+    },
+    safety:{
+        type:Number,
+        required:true
+    },
+    avgIncome:{
         type:Number,
         required:true
     }
@@ -26,5 +126,5 @@ const schoolSchema = new mongoose.Schema({
 });
 
 
-let school= mongoose.model.school || mongoose.model('school', schoolSchema);
-export default school;
+let schools= mongoose.models.school || mongoose.model('school', schoolSchema);
+export default schools;
